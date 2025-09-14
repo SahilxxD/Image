6594473f7643382077b89b5e41dbb9b0ac5b4ade
Sahil Khairnar <sahilkhairnar2786@gmail.com>
2025-09-14 12:26:52 +0530

    Add safelist patterns for Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,14 @@ module.exports = {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
+    safelist: [
+        {
+            pattern: /(bg|text|border)-(red|blue|green|purple|gray)-(100|200|300|400|500|600|700)/,
+        },
+        {
+            pattern: /col-span-(1|2|3|4|5|6|7|8|9|10|11|12)/,
+        },
+    ],
     theme: {
         extend: {},
     },
