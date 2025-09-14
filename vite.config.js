d2c0d9762f9d416317d34ee3b79fe50542e2b8f7
Sahil Khairnar <sahilkhairnar2786@gmail.com>
2025-09-14 12:59:28 +0530

    Update base path configuration in Vite setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || '/Image',
   server: {
     allowedHosts: [
       'b29a2ea76bce.ngrok-free.app', // 👈 your ngrok domain
